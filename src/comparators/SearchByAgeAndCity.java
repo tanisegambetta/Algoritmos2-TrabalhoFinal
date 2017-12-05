9072424ae8f244b1973ba59bea97181324b7a49c
Rafael Jeffman <rafasgj@gmail.com>
2017-12-05 18:55:30 -0300

    Corrigida interface do metodo Search.

--- src/comparators/SearchByAgeAndCity.java
+++ src/comparators/SearchByAgeAndCity.java
@@ -6,10 +6,6 @@ import model.Aluno;
 
 public class SearchByAgeAndCity implements Comparator<Aluno> {
 
-	public SearchByAgeAndCity(int idade, String cidade) {
-		// TODO Auto-generated constructor stub
-	}
-
 	@Override
 	public int compare(Aluno o1, Aluno o2) {
 		// TODO Auto-generated method stub
