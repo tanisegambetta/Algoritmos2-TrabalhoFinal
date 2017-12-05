bf7e54078919af5ded1c45669dc8a581beb7da4e
Rafael Jeffman <rafasgj@gmail.com>
2017-12-05 18:59:44 -0300

    Corrigida interface do metodo Search.

--- src/comparators/SearchByName.java
+++ src/comparators/SearchByName.java
@@ -6,10 +6,6 @@ import model.Aluno;
 
 public class SearchByName implements Comparator<Aluno> {
 
-	public SearchByName(String name) {
-		// TODO Auto-generated constructor stub
-	}
-
 	@Override
 	public int compare(Aluno o1, Aluno o2) {
 		// TODO Auto-generated method stub
