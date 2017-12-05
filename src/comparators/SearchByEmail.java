4b5e423c7007eda48f0822bf45bc985c6e3841bb
Rafael Jeffman <rafasgj@gmail.com>
2017-12-05 18:58:43 -0300

    Corrigida interface do metodo Search.

--- src/comparators/SearchByEmail.java
+++ src/comparators/SearchByEmail.java
@@ -6,10 +6,6 @@ import model.Aluno;
 
 public class SearchByEmail implements Comparator<Aluno> {
 
-	public SearchByEmail(String email) {
-		// TODO Auto-generated constructor stub
-	}
-
 	@Override
 	public int compare(Aluno o1, Aluno o2) {
 		// TODO Auto-generated method stub
